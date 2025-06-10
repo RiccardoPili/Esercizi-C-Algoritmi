2d7ec47038fc4b0d7a905a4bb473b7dbf603e366
riccardo.pili@edu.unito.it <riccardopili01@gmail.com>
2025-06-10 09:37:45 +0200

    setting json

--- list.c
+++ list.c
@@ -1,17 +1,17 @@
 /*****************************************
-*  Struttura dati per le liste semplici
-*  di interi
-******************************************/
+ *  Struttura dati per le liste semplici
+ *  di interi
+ ******************************************/
 
+#include <ctype.h>
+#include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
-#include <stdbool.h>
-#include <math.h>
 
 struct listEl {
-    int            info;
+    int info;
     struct listEl* next;
 };
 
@@ -24,7 +24,7 @@ list Cons(int x, list xs) {
     return newlist;
 }
 
-void printlist (list l) {
+void printlist(list l) {
     while (l != NULL) {
         printf("%d ", l->info);
         l = l->next;
@@ -32,12 +32,11 @@ void printlist (list l) {
     printf("\n");
 }
 
-
 // ------------------ METODI AUSILIARI ------------------------------
 
-list ArrayToList(int A[], int n){
+list ArrayToList(int A[], int n) {
     list l = NULL;
-    for(int i=n-1; i>=0; i--){
+    for (int i = n - 1; i >= 0; i--) {
         l = Cons(A[i], l);
     }
     return l;
@@ -51,9 +50,9 @@ list copylist(list l) {
     }
 }
 
-list reverse(list l){
+list reverse(list l) {
     list res = NULL;
-    while(l != NULL){
+    while (l != NULL) {
         res = Cons(l->info, res);
         l = l->next;
     }
@@ -63,45 +62,40 @@ list reverse(list l){
 list concat(list a, list b) {
     list result = b;
     list a_reversed = reverse(a);
-    while (a_reversed != NULL){
+    while (a_reversed != NULL) {
         result = Cons(a_reversed->info, result);
         a_reversed = a_reversed->next;
     }
     return result;
 }
 
-
-
-
-
-
 // ------------------ ESERCIZI SU LISTE -------------------------
 
-int sum_list (list l) {
+int sum_list(list l) {
     int sum = 0;
-    while(l!=NULL){
+    while (l != NULL) {
         sum += l->info;
         l = l->next;
     }
     return sum;
 }
 
-int sum_list_rec (list l) {
-    if(l==NULL) return 0;
-    else{
+int sum_list_rec(list l) {
+    if (l == NULL)
+        return 0;
+    else {
         return l->info + sum_list_rec(l->next);
     }
 }
-
 
 /*
 Lista ordinata
     pre: l lista di interi
     post: restituisce true se la lista è ordinata non decrescente, false altrimenti
 */
-bool is_ordered(list l){
-    while(l->next != NULL){
-        if(l->info > l->next->info) return false;
+bool is_ordered(list l) {
+    while (l->next != NULL) {
+        if (l->info > l->next->info) return false;
         l = l->next;
     }
     return true;
@@ -109,36 +103,33 @@ bool is_ordered(list l){
 
 /*
 Cancellazione
-    pre: x elemento/i da eliminare, l lista di interi 
+    pre: x elemento/i da eliminare, l lista di interi
     post: lista modificata eliminando tutte le occorrenze di x dalla lista l
 */
-list delete_all(int x, list l){
-    if(l == NULL) return l;
+list delete_all(int x, list l) {
+    if (l == NULL) return l;
 
-    if(l->info == x){
+    if (l->info == x) {
         return delete_all(x, l->next);
-    }
-    else{
+    } else {
         l->next = delete_all(x, l->next);
         return l;
     }
 }
 
-
 /*
-Unione di Liste 
+Unione di Liste
     pre: l, m liste ordinate crescenti di interi
     post: restituisce una nuova lista ordinata crescente unione delle liste passate come parametro
     note: l'algoritmo deve avere complessità O(n)
 */
-list unione(list l, list m){
-    if(l == NULL) return m;
-    if(m == NULL) return l;
+list unione(list l, list m) {
+    if (l == NULL) return m;
+    if (m == NULL) return l;
 
-    if(l->info <= m->info){
+    if (l->info <= m->info) {
         return Cons(l->info, unione(l->next, m));
-    }
-    else{
+    } else {
         return Cons(m->info, unione(l, m->next));
     }
 }
@@ -149,17 +140,15 @@ Unione senza duplicati
     post: restituisce una nuova lista ordinata crescente unione delle liste passate come parametro,
     la lista non deve contenere elementi duplicati
 */
-list unione_no_dup(list l, list m){
-    if(l == NULL) return m;
-    if(m == NULL) return l;
+list unione_no_dup(list l, list m) {
+    if (l == NULL) return m;
+    if (m == NULL) return l;
 
-    if(l->info < m->info){
+    if (l->info < m->info) {
         return Cons(l->info, unione_no_dup(l->next, m));
-    }
-    else if(l->info > m->info){
+    } else if (l->info > m->info) {
         return Cons(m->info, unione_no_dup(l, m->next));
-    }
-    else{
+    } else {
         return Cons(l->info, unione_no_dup(l->next, m->next));
     }
 }
@@ -171,19 +160,17 @@ Differenza tra Liste
     note: usare copylist()
     esempio: l = {2 3 4 6} m = {1 2 5 7 8} risultato = {3 4 6}
 */
-list diff(list l, list m){
+list diff(list l, list m) {
     list L = copylist(l);
     list M = copylist(m);
-    if(L == NULL) return NULL;
-    if(M == NULL) return L;
-    
-    if(L->info < M->info){
+    if (L == NULL) return NULL;
+    if (M == NULL) return L;
+
+    if (L->info < M->info) {
         return Cons(L->info, diff(L->next, M));
-    }
-    else if (L->info == M->info){
+    } else if (L->info == M->info) {
         return diff(L->next, M->next);
-    }
-    else{
+    } else {
         return diff(L, M->next);
     }
 }
@@ -193,37 +180,32 @@ Sottoinsiemi con liste
     pre: l, m liste ordinate crescenti
     post: true se tutti gli elementi di l sono inclusi in m
 */
-bool subset(list l, list m){
-    if(l == NULL) return true;
-    if(m == NULL) return false;
-    
-    if(l->info == m->info){
+bool subset(list l, list m) {
+    if (l == NULL) return true;
+    if (m == NULL) return false;
+
+    if (l->info == m->info) {
         return subset(l->next, m->next);
-    }
-    else if(l->info > m->info){
+    } else if (l->info > m->info) {
         return subset(l, m->next);
-    }
-    else{
+    } else {
         return false;
     }
 }
-
 
 /*
 Intersezioni con liste
     pre: l, m liste ordinate crescenti
     post: la lista degli elementi in comune tra l e m in ordine crescente
 */
-list intersect(list l, list m){
-    if(l == NULL || m == NULL) return NULL; //intersezione vuota
+list intersect(list l, list m) {
+    if (l == NULL || m == NULL) return NULL;  // intersezione vuota
 
-    if(l->info == m->info){
+    if (l->info == m->info) {
         return Cons(l->info, intersect(l->next, m->next));
-    }
-    else if(l->info < m->info){
+    } else if (l->info < m->info) {
         return intersect(l->next, m);
-    }
-    else{
+    } else {
         return intersect(l, m->next);
     }
 }
@@ -238,12 +220,10 @@ Rango di una Lista
        0 altrimenti
     esempio: [1, 2, 3] diventa [6, 5, 3]
 */
-int rank(list l) 
-{
-    if(l == NULL)
+int rank(list l) {
+    if (l == NULL)
         return 0;
-    else
-    {
+    else {
         l->info = l->info + rank(l->next);
         return l->info;
     }
@@ -254,12 +234,13 @@ MergeSort con Liste
     scrivere la funzione di fusione dell'algoritmo merge sort visto a lezione,
     ma utilizzando come struttura dati le liste invece che gli array
 */
-list merge(list l, list m){
-    if(l == NULL) return m;
-    if(m == NULL) return l;
+list merge(list l, list m) {
+    if (l == NULL) return m;
+    if (m == NULL)
+        return l;
 
-    else{
-        if(l->info <= m->info){
+    else {
+        if (l->info <= m->info) {
             l->next = merge(l->next, m);
             return l;
         } else {
@@ -275,10 +256,10 @@ Lista palindroma
     esempio:    [1, 2, 25, 36, 25, 2, 1] -> true
                 [1, 2, 25, 40, 36, 2, 1] -> false
 */
-bool palindrome(list l){
+bool palindrome(list l) {
     list rev = reverse(l);
-    while(l != NULL) {
-        if(l->info != rev->info) {
+    while (l != NULL) {
+        if (l->info != rev->info) {
             return false;
         }
         l = l->next;
@@ -293,16 +274,16 @@ Lista palindroma (senza usare reverse)
     esempio:    [1, 2, 25, 36, 25, 2, 1] -> true
                 [1, 2, 25, 40, 36, 2, 1] -> false
 */
-bool palindrome_no_reverse(list l){
+bool palindrome_no_reverse(list l) {
     list rev = NULL;
     list temp = l;
 
-    while(temp != NULL){
+    while (temp != NULL) {
         rev = Cons(temp->info, rev);
         temp = temp->next;
     }
-    while(l != NULL){
-        if(l->info != rev->info){
+    while (l != NULL) {
+        if (l->info != rev->info) {
             return false;
         }
         l = l->next;
@@ -311,7 +292,6 @@ bool palindrome_no_reverse(list l){
     return true;
 }
 
-
 /*
 OddEven
     scrivere una funzione che riordini la lista passata come parametro in modo che tutti
@@ -319,11 +299,11 @@ OddEven
     esempio: [1, 4, 5, 6, 7, 2] diventa [1, 5, 7, 4, 6, 2]
     note: usare funzioni ausiliarie concat e reverse
 */
-list oddEven(list l){
+list oddEven(list l) {
     list dispari = NULL;
     list pari = NULL;
 
-    while(l != NULL){
+    while (l != NULL) {
         if (l->info % 2 != 0) {
             dispari = Cons(l->info, dispari);
         } else {
@@ -331,51 +311,49 @@ list oddEven(list l){
         }
         l = l->next;
     }
-    
+
     return concat(reverse(dispari), reverse(pari));
 }
-
-
 
 // ------------------------- MAIN ---------------------------
 int main() {
     int arr[] = {10, 5, 8, 2, 16, 12};
-    list my_list = ArrayToList(arr, sizeof(arr)/sizeof(int));
+    list my_list = ArrayToList(arr, sizeof(arr) / sizeof(int));
     printlist(my_list);
     printf("Somma: %d\n", sum_list(my_list));
     printf("Somma ricorsiva: %d\n", sum_list_rec(my_list));
 
     printf("\nUnione di liste\n");
     int a1[] = {1, 2, 3, 4, 6, 10};
-    list l1 = ArrayToList(a1, sizeof(a1)/sizeof(int));
+    list l1 = ArrayToList(a1, sizeof(a1) / sizeof(int));
     printlist(l1);
     int a2[] = {2, 4, 5, 10};
-    list l2 = ArrayToList(a2, sizeof(a2)/sizeof(int));
+    list l2 = ArrayToList(a2, sizeof(a2) / sizeof(int));
     printlist(l2);
     printlist(unione(l1, l2));
     printlist(unione_no_dup(l1, l2));
 
     printf("\nInversione di una lista\n");
     int a3[] = {1, 2, 3, 4, 6, 10};
-    list l3 = ArrayToList(a3, sizeof(a3)/sizeof(int));
+    list l3 = ArrayToList(a3, sizeof(a3) / sizeof(int));
     printlist(l3);
     printlist(reverse(l3));
 
     int a4[] = {1, 2, 3, 4, 7, 10};
-    list l4 = ArrayToList(a4, sizeof(a4)/sizeof(int));
+    list l4 = ArrayToList(a4, sizeof(a4) / sizeof(int));
     printf("\nIs ordered? %d\n", is_ordered(l4));
 
     printf("\nRimozione di tutti i 2 dalla lista\n");
     int a5[] = {1, 2, 3, 4, 2, 6, 2, 10};
-    list l5 = ArrayToList(a5, sizeof(a5)/sizeof(int));
+    list l5 = ArrayToList(a5, sizeof(a5) / sizeof(int));
     printlist(l5);
     printlist(delete_all(2, l5));
 
     printf("\nOperazioni su Liste:\n");
     int arrA[] = {1, 2, 3, 4, 6, 10};
     int arrB[] = {2, 4, 5, 10};
-    list A = ArrayToList(arrA, sizeof(arrA)/sizeof(int));
-    list B = ArrayToList(arrB, sizeof(arrB)/sizeof(int));
+    list A = ArrayToList(arrA, sizeof(arrA) / sizeof(int));
+    list B = ArrayToList(arrB, sizeof(arrB) / sizeof(int));
 
     printf("Lista A: ");
     printlist(A);
@@ -394,8 +372,8 @@ int main() {
     printf("\nLista Palindroma\n");
     int pal[] = {1, 5, 3, 4, 3, 5, 1};
     int not_pal[] = {10, 5, 8, 2, 8, 10};
-    list list_pal = ArrayToList(pal, sizeof(pal)/sizeof(int));
-    list list_not_pal = ArrayToList(not_pal, sizeof(not_pal)/sizeof(int));
+    list list_pal = ArrayToList(pal, sizeof(pal) / sizeof(int));
+    list list_not_pal = ArrayToList(not_pal, sizeof(not_pal) / sizeof(int));
     printlist(list_pal);
     printlist(list_not_pal);
     printf("palindrome -> pal: %d, not_pal: %d\n", palindrome(list_pal), palindrome(list_not_pal));
